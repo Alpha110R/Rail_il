971da934356a754d4f47fcef220f50f47b6e0b6f
afeka <afeka@Ubuntu19V4-2.academic.management.afeka.local>
2020-08-03 15:47:30 +0300

    3- The project

---
+++ workspace/Rail_il/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Scanner s = new Scanner (System.in);
+		int choice;
+		do{
+			System.out.println("Please select an option:");
+			System.out.println("1 - Type your ride details");
+			System.out.println("2 - Show ride details");
+			System.out.println("9 - Exit");
+			System.out.println("Enter your choice --> ");
+			choice = s.nextInt();
+			switch(choice){
+			case 1: String station;
+				
+			}
+
+			
+		}while(choice !=9);
+		
+	}
+
+}
