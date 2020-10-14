09308ad5948a93dc9f80a9b6f04a3556b0a0f3d5
Tom Firer <47198574+tomfirer@users.noreply.github.com>
2020-10-14 08:49:10 +0300

    sprint 3 - python file

---
+++ app.py
@@ -0,0 +1,17 @@
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+#!/usr/bin/python
+import subprocess
+
+from flask import Flask, request
+
+app = Flask("my_app1")
+
+@app.route("/rail")
+def search_rail():
+    start_destination = str(request.args.get('startStop'))
+    start_time = str(request.args.get('startTime'))
+    end_destination = str(request.args.get('endStop'))
+    return subprocess.check_output(
+        ["java", "-classpath", "/home/afeka/workspace/Rails_IL/bin", "Rails_IL/UserMain", start_destination, start_time,
+         end_destination])
+
